c78488e9bbd8450f4d862a04ad0e6fcf6db62ffc
olavhl <ohalar@gmail.com>
2021-04-26 09:23:58 +0200

    Initial commit. Set up with both client and server.

---
+++ src/client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Application() {
+    return <h1>Hello</h1>;
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"));
