d9be2064686034b75f4129e2f21a64b728341b7b
olavhl <ohalar@gmail.com>
2021-04-26 20:39:26 +0200

    Possible to chat with yourself

--- src/client/ChatPage.jsx
+++ src/client/ChatPage.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function ChatPage() {
+  // const ws = new WebSocket();
+  // ws.onmessage;
+
+  const [chat, setChat] = useState([]);
+  const [message, setMessage] = useState("");
+
+  function handleSubmitChat(e) {
+    e.preventDefault();
+    setChat([...chat, message]);
+    setMessage("");
+  }
+
   return (
     <div className={"chat"}>
       <header>
         <h1>Chat</h1>
       </header>
       <main>
-        <h2>Chat started</h2>
+        {chat?.map((message, index) => (
+          <div key={`${message}__${index}`}>{message}</div>
+        ))}
       </main>
       <footer>
-        <form>
-          <input type="text" />
+        <form onSubmit={handleSubmitChat}>
+          <input
+            type="text"
+            autoFocus={true}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
           <button>send</button>
         </form>
       </footer>
