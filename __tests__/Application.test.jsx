2d6968866290818778150d3d82097bc9d4742fda
olavhl <ohalar@gmail.com>
2021-04-27 17:39:02 +0200

    Testing more of the server

--- __tests__/Application.test.jsx
+++ __tests__/Application.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Application } from "../src/client/Application";
 import { MemoryRouter } from "react-router";
+import { act } from "react-dom/test-utils";
 
 describe("application", () => {
   it("shows application", async () => {
@@ -15,5 +16,32 @@ describe("application", () => {
 
     expect(container.innerHTML).toMatchSnapshot();
     expect(container.querySelector("h1").textContent).toEqual("Welcome");
+  });
+
+  it("can navigate to user page", async () => {
+    const container = document.createElement("div");
+    await act(async () => {
+      await ReactDOM.render(
+        <MemoryRouter>
+          <Application />
+        </MemoryRouter>,
+        container
+      );
+    });
+
+    const userPageLink = [...container.querySelectorAll("a")].find(
+      (a) => a.textContent === "Users"
+    );
+
+    await act(async () => {
+      await userPageLink.dispatchEvent(
+        new MouseEvent("click", { bubbles: true })
+      );
+    });
+
+    expect(container.innerHTML).toMatchSnapshot();
+    expect(container.querySelector("h1").textContent).toEqual(
+      "User interactions"
+    );
   });
 });
